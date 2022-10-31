312c8ac685854cf1672e63574d110573e29e8f6a
Arina M <apelsinka7.ru@yandex.ru>
2022-10-31 16:43:19 +0300

    программа для решения задачи

---
+++ FinalZadanie/Program.cs
@@ -0,0 +1,33 @@
+﻿string[] array = new string [7] {"Hello", "2", "world", "-2", "1234", "1567", "computer_science"};
+string[] arrayFinal = new string[array.Length];
+
+
+void PrintArray(string[] array)
+{
+for (int i = 0; i < array.Length; i++)
+{
+        Console.Write($"{array[i] + "  "} ");
+}
+    Console.WriteLine();
+}
+
+
+void NewArray(string[] array, string[] arrayFinal)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+    if(array[i].Length <= 3)
+        {
+        arrayFinal[count] = array[i];
+        count++;
+        }
+    }
+}
+Console.Write($"Задан массив: ");
+PrintArray(array);
+Console.WriteLine();
+
+Console.Write($"Новый массив: ");
+NewArray(array, arrayFinal);
+PrintArray(arrayFinal);
